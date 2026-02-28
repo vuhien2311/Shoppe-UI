f1f6cc138bac9f18391f25066f4716bed4d9c62e
vuhien2311 <vuhien7789@gmail.com>
2026-03-01 00:30:11 +0700

    Update frontend backend url

--- Ba╠ën sao shopee convert 2/assets/js/config.js
+++ Ba╠ën sao shopee convert 2/assets/js/config.js
@@ -1,6 +1,6 @@
 window.ShopeeConfig = {
-  APP_VERSION: "2026.02.28.7",
-  BACKEND_BASE_URL: "http://127.0.0.1:8787",
+  APP_VERSION: "2026.03.01.1",
+  BACKEND_BASE_URL: "https://bb46-2405-4803-ff61-5710-d9ad-de69-d39f-f541.ngrok-free.app",
   JOB_TIMEOUT_MS: 120000,
   JOB_POLL_MS: 400,
   MIN_CONVERT_INTERVAL_MS: 700,
