f72f1b77c43fb5284b8499ce0f6f2b29428a23df
vuhien2311 <vuhien7789@gmail.com>
2026-03-01 13:46:11 +0700

    Use Cloudflare Tunnel url

--- Ba╠ën sao shopee convert 2/assets/js/config.js
+++ Ba╠ën sao shopee convert 2/assets/js/config.js
@@ -1,6 +1,6 @@
 window.ShopeeConfig = {
-  APP_VERSION: "2026.03.01.2",
-  BACKEND_BASE_URL: "https://bb46-2405-4803-ff61-5710-d9ad-de69-d39f-f541.ngrok-free.app",
+  APP_VERSION: "2026.03.01.3",
+  BACKEND_BASE_URL: "https://news-traveling-publicly-resolved.trycloudflare.com",
   JOB_TIMEOUT_MS: 120000,
   JOB_POLL_MS: 400,
   MIN_CONVERT_INTERVAL_MS: 700,
